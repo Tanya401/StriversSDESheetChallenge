8a206d5dd26cd359db63a7e08baed3e99502b772
Tanya Ahuja <85565783+Tanya401@users.noreply.github.com>
2023-07-06 17:53:18 +0530

    Add files via upload

---
+++ queue using stack.cpp
@@ -0,0 +1,53 @@
+#include<stack>
+class Queue {
+    // Define the data members(if any) here.
+    stack<int> s1,s2;
+    public:
+    Queue() {
+        // Initialize your data structure here.
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        s1.push(val);
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        if(s1.size()==0) return -1;
+        while(s1.size()>1){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ans = s1.top();
+        s1.pop();
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ans;
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+         if(s1.size()==0) return -1;
+        while(s1.size()>1){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ans = s1.top();
+        s1.pop();
+        s2.push(ans);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ans;
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function here.
+        if(s1.size()==0) return 1;
+        return 0;
+    }
+};
