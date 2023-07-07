811bb0c1e33587b5766098c4e51b8270c67e9318
Tanya Ahuja <85565783+Tanya401@users.noreply.github.com>
2023-07-07 13:25:14 +0530

    Add files via upload

---
+++ Identical trees.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+bool find(BinaryTreeNode<int>*root1,BinaryTreeNode<int>*root2){
+    if(!root1 && !root2) return true;
+    if(!root1 || !root2) return false;
+
+    return ((root1->data==root2->data) && find(root1->left,root2->left) && find(root1->right,root2->right));
+}
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    // Write your code here. 
+    return find(root1,root2);	 
+}
