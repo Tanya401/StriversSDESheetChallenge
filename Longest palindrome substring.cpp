7abac24ce1797824cc4b0623359491d118aee4d4
Tanya Ahuja <85565783+Tanya401@users.noreply.github.com>
2023-07-12 21:50:57 +0530

    Add files via upload

---
+++ Longest palindrome substring.cpp
@@ -0,0 +1,33 @@
+string longestPalinSubstring(string str)
+{
+    // Write your code here.
+    int low,high;
+    int start =0 ,end = 1;
+    for(int i=1;i<str.size();i++){
+        //even part
+        low = i-1;
+        high = i;
+   while(low>=0 && high<str.size() && str[low]==str[high]){
+       if(high-low+1 > end){
+           start = low;
+           end = high-low+1;
+       }
+       low--;
+       high++;
+   }
+
+   //odd part
+    low = i-1;
+        high = i+1;
+   while(low>=0 && high<str.size() && str[low]==str[high]){
+       if(high-low+1 > end){
+           start = low;
+           end = high-low+1;
+       }
+       low--;
+       high++;
+    }
+    }
+    string temp = str.substr(start,end);
+    return temp;
+    }
