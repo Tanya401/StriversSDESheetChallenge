557823cf1ad9e6466465cd0eeeb6cca44a58a2e7
Tanya Ahuja <85565783+Tanya401@users.noreply.github.com>
2023-07-12 22:33:13 +0530

    Add files via upload

---
+++ Rabin karp.cpp
@@ -0,0 +1,14 @@
+vector<int> stringMatch(string text, string pattern) {
+	// Write your code here.
+	vector<int> ans;
+	int n = pattern.size();
+	for(int i=0;i<text.size();i++){
+		if(text[i]==pattern[0] && (i+n-1)<text.size()){
+			string str = text.substr(i,n);
+			if(str==pattern){
+				ans.push_back(i+1);
+			}
+		}
+	}
+	return ans;
+}
